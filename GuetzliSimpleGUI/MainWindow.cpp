d66039f7ad8aeef9bc7050ace758b1f86be81b59
Oliver Knoll <starky.hutch@gmx.ch>
2017-03-29 21:54:57 +0200

    Provide image preview label.

--- GuetzliSimpleGUI/MainWindow.cpp
+++ GuetzliSimpleGUI/MainWindow.cpp
@@ -1,5 +1,6 @@
 #include <QImageWriter>
 #include <QImage>
+#include <QPixmap>
 #include <QFileDialog>
 
 
@@ -11,11 +12,23 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_image = new QImage();
+    updateUi();
 }
 
 MainWindow::~MainWindow()
 {
+    delete m_image;
     delete ui;
+}
+
+// Private
+
+void MainWindow::updateUi()
+{
+    bool enabled = !m_image->isNull();
+    ui->saveAsAction->setEnabled(enabled);
+    ui->saveButton->setEnabled(enabled);
 }
 
 // Private Slots
@@ -25,30 +38,38 @@ void MainWindow::openImage()
     qDebug("Open Action");
     m_filePath = QFileDialog::getOpenFileName(this, tr("Open"), m_filePath);
     this->setWindowTitle(m_filePath);
+    if (!m_filePath.isNull()) {
+
+        bool success = m_image->load(m_filePath);
+        if (success) {
+            QImage previewImage;
+            if (m_image->width() > ui->imagePreviewLabel->maximumWidth() ||
+                m_image->height() > ui->imagePreviewLabel->maximumHeight()
+               ) {
+                if (m_image->width() > m_image->height()) {
+                    previewImage = m_image->scaledToWidth(ui->imagePreviewLabel->maximumWidth());
+                } else {
+                    previewImage = m_image->scaledToHeight(ui->imagePreviewLabel->maximumHeight());
+                }
+            } else {
+                previewImage = *m_image;
+            }
+            ui->imagePreviewLabel->setPixmap(QPixmap::fromImage(previewImage));
+        }
+    }
+    updateUi();
 }
 
 void MainWindow::saveImage()
 {
-    QList<QByteArray> supportedImageFormats = QImageWriter::supportedImageFormats();
-
-    foreach (QByteArray imageFormat, supportedImageFormats) {
-        qDebug("%s", qPrintable(imageFormat));
-    }
-
-    QImage image;
-
-    if (!m_filePath.isEmpty()) {
-        bool res = image.load(this->m_filePath, "jpg");
-        qDebug("Image load success: %d", res);
-
+    if (!m_image->isNull()) {
         QString saveFilePath = QFileDialog::getSaveFileName(this, tr("Save"), m_filePath);
 
         QImageWriter imageWriter;
         imageWriter.setFileName(saveFilePath);
         imageWriter.setFormat("guetzli");
         imageWriter.setQuality(85);
-        imageWriter.write(image);
+        imageWriter.write(*m_image);
     }
-
 }
 
